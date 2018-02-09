bd685f32051f912a9423c861ee7f4ca26eaf26ec
Simon Nattrass <simon.nattrass@avaloq.com>
2018-02-09 14:38:51 +0000

    Adjusting Image Name, Port Mappings and HTTP Proxy
    - Add SMIA_USER env variable to the image name.
    - Hard code port mappings for Docker host ports.
    - Provide Config Service with HTTP_PROXY which is required to connect
    to github.com (actually HTTPS variant is needed, but we're specifying
    both for completeness).

--- chapter3/config-service/src/main/docker/run.sh
+++ chapter3/config-service/src/main/docker/run.sh
@@ -2,4 +2,5 @@
 echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
+export _JAVA_OPTIONS="-Dhttp.proxyHost=proxy.sys.net -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.sys.net -Dhttps.proxyPort=8080"
 java -jar /usr/local/config-service/config-service-0.0.1-SNAPSHOT.jar
