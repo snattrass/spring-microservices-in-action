870e9f0b0f7a4e1e336c433f3d697d253921bae5
Simon Nattrass <simon@nattrass.io>
2018-02-20 15:59:37 +0000

    More renaming and hyphenation

--- chapter4/eureka-service/src/main/docker/run.sh
+++ chapter4/eureka-service/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/eurekasvr-0.0.1-SNAPSHOT.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/eureka-service-0.0.1-SNAPSHOT.jar
