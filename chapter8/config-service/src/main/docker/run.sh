7e3d5be0dff8f06992ee08422828ebc314bc25ca
Simon Nattrass <simon@nattrass.io>
2018-04-21 19:15:04 +0100

    Avaloqizing chapter8

--- chapter8/config-service/src/main/docker/run.sh
+++ chapter8/config-service/src/main/docker/run.sh
@@ -8,4 +8,5 @@ echo "******* Eureka Server has started"
 echo "********************************************************"
 echo "Starting Configuration Service with Eureka Endpoint:  $EUREKASERVER_URI";
 echo "********************************************************"
+export _JAVA_OPTIONS="-Dhttp.proxyHost=proxy.sys.net -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.sys.net -Dhttps.proxyPort=8080"
 java -Djava.security.egd=file:/dev/./urandom -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configserver/config-service-0.0.1-SNAPSHOT.jar
